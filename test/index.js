5792ff0235318d588461d812aeaf2734a1e0dad9
Tyler Long <tyler4long@gmail.com>
2019-11-01 19:28:44 -0700

    Add test file

---
+++ test/index.js
@@ -0,0 +1,23 @@
+import { nonstandard } from 'wrtc'
+import fs from 'fs'
+
+import NodeWebRtcAudioStreamSource from '../src'
+
+const rtcAudioStreamSource = new NodeWebRtcAudioStreamSource()
+
+const track = rtcAudioStreamSource.createTrack()
+const sink = new nonstandard.RTCAudioSink(track)
+
+const audioFilePath = 'temp.tiff'
+if (fs.existsSync(audioFilePath)) {
+  fs.unlinkSync(audioFilePath)
+}
+const writeStream = fs.createWriteStream(audioFilePath, { flags: 'a' })
+sink.ondata = data => {
+  writeStream.write(Buffer.from(data.samples.buffer))
+}
+
+const readStream = fs.createReadStream('test.tiff')
+rtcAudioStreamSource.addStream(readStream)
+
+setTimeout(() => console.log('done'), 1000)
